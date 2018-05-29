f0df853728806c0efa27482a5ae33793500b2960
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 12:47:47 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   root 'sessions#create'
   resources :sessions
   post '/logout' => 'sessions#destroy'
+  get '/secrets' => 'secrets#show'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
