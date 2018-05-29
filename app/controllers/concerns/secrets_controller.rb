d7ee584b0f5e9161d5f09ff77c2357ed7a0b800a
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 12:49:50 +0000

    automatically backed up by learn

--- app/controllers/concerns/secrets_controller.rb
+++ app/controllers/concerns/secrets_controller.rb
@@ -1,5 +1,12 @@
 class SecretsController < ApplicationController
-
+  before_action 
   def show
+    if current_user 
+  end
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :name
   end
 end
