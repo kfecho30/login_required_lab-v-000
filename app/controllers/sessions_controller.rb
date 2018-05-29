403d74908faa15f119c3ecc65ff8e730a0847cb2
Kelsey Fecho <kfecho30@gmail.com>
2018-05-29 12:41:36 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     if params[:name].nil? || params[:name].empty?
-      redirect_to 'sessions/new'
+      redirect_to '/sessions/new'
     end
   end
 
   def destroy
     session.destroy
-    redirect_to 'sessions/new'
+    redirect_to '/sessions/new'
   end
 end
